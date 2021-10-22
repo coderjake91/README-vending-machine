24b36d163d3a3ee04ff1bcb205ce0f9143c30d50
coderjake91 <jacob.b.frazer@gmail.com>
2021-10-21 22:41:02 -0500

    completed basic CLI function using inquirer prompts, and checked re-contextualized fs writeFile method to see if it was functional, installed npm inquirer package

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const mockData = {
 const collectREADMEInfo = () => {
 
     console.log(`
-    ===================================
-     Welcome to README Vending Machine
-    ===================================
+    =======================================
+     Welcome to the README Vending Machine
+    =======================================
 
     Let's generate your project's custom README!
     `);
@@ -60,9 +60,7 @@ const collectREADMEInfo = () => {
         {
             type: 'input',
             name: 'usage',
-            message: `Please provide instructions for app use (Required).
-                      You can also include a screenshot to your README (if desired) by adding the screeshot
-                      to the assets/images folder in the application directory`,
+            message: 'Please provide instructions for app use (Required). You can also include a screenshot to your README (if desired) by adding the screeshot to the assets/images folder in the application directory',
             validate: usage => {
                 if(!usage){
                     console.log('Please enter usage instructions for your application!');
@@ -97,12 +95,55 @@ const collectContributors = (infoREADME) => {
     
     if(!infoREADME.contributors) {
         infoREADME.contributors = [];
+        loopMessage = 'a contributor';
+    } else {
+        loopMessage = 'another contributor'
     }
 
     return inquirer.prompt([
         //prompt user for their contributors to the project (confirm if they want to keep adding)
+        {
+            type: 'confirm',
+            name: 'confirmContributor',
+            message: `Would you like to add ${loopMessage} to your project?`,
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'profileName',
+            message: "Please enter the contributor's github profile name",
+            when: ({confirmContributor}) => {
+                if(confirmContributor){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
     ])
-    .then(contributors => {
+    .then(contributor => {
         //append each contributor to the infoREADME.contributors array, then call collectContributors again (if confirm === true)
-    })
+        infoREADME.contributors.push(contributor);
+        if(contributor.confirmContributor){
+            return collectContributors(infoREADME);
+        } else {
+            return infoREADME;
+        }
+    });
 }
+
+collectREADMEInfo()
+    .then(collectContributors)
+    .then(infoREADME => {
+        //return generateREADME(infoREADME);
+        return JSON.stringify(infoREADME);
+    })
+    .then(markdown => {
+        return writeFile(markdown);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+    })
+    .catch(err => {
+       console.log(err); 
+    });
